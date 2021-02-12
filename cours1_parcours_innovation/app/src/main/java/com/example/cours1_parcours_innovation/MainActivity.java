30c45d83d01eb4d128fc78676132cdabdf283c54
clementreiffers <reiffersclement@gmail.com>
2021-02-12 14:12:00 +0100

    ajout du premier cours android

---
+++ cours1_parcours_innovation/app/src/main/java/com/example/cours1_parcours_innovation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cours1_parcours_innovation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
