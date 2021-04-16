60a764d3ede43e874a16d97c7ef9371ea7e6b594
clementreiffers <reiffersclement@gmail.com>
2021-04-16 16:56:48 +0200

    affichage des artistes ok, reste plus que l'affichage des musiques par artistes

---
+++ Wolfii/app/src/main/java/com/example/wolfii/MyArtisteAdapter.java
@@ -0,0 +1,105 @@
+package com.example.wolfii;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyArtisteAdapter extends RecyclerView.Adapter<MyArtisteAdapter.MyViewHolder> {
+    // classe qui est responsable de chaque cellule
+    // responsable du recyclage des view
+    // view holder = accelerer le rendu de la liste, il sera déclaré au sein de l'adapter
+    List<String> mesArtistes;
+
+
+    public void setMesMusiques(List<String> mesArtistes) {
+        this.mesArtistes = mesArtistes;
+    }
+
+    public MyArtisteAdapter(ArrayList<String> mesArtistes) {
+        this.mesArtistes = mesArtistes;
+    }
+
+    public Object getItem(int position) {
+        return mesArtistes.get(position);
+    }
+
+    public interface MusiqueItemClickListener {
+        void onMusiqueItemClick(View view, Musique musique, int position);
+        void onMusiqueItemLongClick(View view, Musique musique, int position);
+    }
+    private MusiqueItemClickListener mMusiqueItemClickListener;
+
+    public void setmMusiqueItemClickListener(MusiqueItemClickListener mMusiqueItemClickListener) {
+        this.mMusiqueItemClickListener = mMusiqueItemClickListener;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // on cherche notre vue avec inflater
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        // on va chercher notre layout
+        View view = layoutInflater.inflate(R.layout.musique_item, parent, false);
+        // on renvoie le viewholder
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        // affiche les viewholder en donnant la position
+        holder.display(mesArtistes.get(position));
+        Log.d("position", position + "");
+    }
+
+    @Override
+    public int getItemCount() {
+        return mesArtistes.size(); // pour ne pas être embete avec les tailles de liste
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView mName;
+
+        public MyViewHolder(@NonNull View itemView) {
+            // itemview = vue de chaque cellule
+            super(itemView);
+
+            // afficher le nom de la musique courante
+            mName = (TextView) itemView.findViewById(R.id.name);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mMusiqueItemClickListener != null) {
+                        mMusiqueItemClickListener.onMusiqueItemClick(
+                                itemView,
+                                (Musique) getItem(getAdapterPosition()),
+                                getAdapterPosition());
+                    }
+                }
+            });
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    if (mMusiqueItemClickListener != null) {
+                        mMusiqueItemClickListener.onMusiqueItemLongClick(
+                                itemView,
+                                (Musique)getItem(getAdapterPosition()),
+                                getAdapterPosition());
+                    }
+                    return false;
+                }
+            });
+        }
+        void display(String artiste) {
+            // ne jamais le mettre dans le constructeur
+            mName.setText(artiste);
+        }
+
+    }
+}
