f41b8d031952eaa26d18c50b3216e771396af8b3
Im-Rises <Quentin-morel88@hotmail.com>
2021-04-18 10:20:00 +0200

    Suppression du code d'un second Bound Service de connexion

--- Wolfii/app/src/main/java/com/example/wolfii/ListAllArtistsFragment.java
+++ Wolfii/app/src/main/java/com/example/wolfii/ListAllArtistsFragment.java
@@ -26,26 +26,12 @@ public class ListAllArtistsFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private ArrayList<String> mesArtistes;
     private MyArtisteAdapter monAdapter;
-    private boolean mBound = false;
-    private MusiqueService mService;
-
-/*
-* ----------------------------------------------ATTENTION POUR CLEMENT-------------------------------------------------------------------------
-* Je n'ai rien modifié, ce message est là juste pour t'informer de :
-* Déso d'écrire ici mais c'est pour te rappeler qu'il faut supprimer le mService et mBound ici, on s'en fiche
-* il faut utiliser celui du mainActivity ! Car sinon on le déclare deux fois !!! Surtout que tu gères pas son arrêt donc il ya des fuites de mémoire !
-*
- */
 
 
     @SuppressLint("WrongConstant")
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_mes_artistes, container, false);
-
-        getActivity().startService(new Intent(getActivity(), MusiqueService.class));
-        Intent intent = new Intent(getActivity(), MusiqueService.class);
-        getActivity().bindService(intent, connection, 0);
 
         // creation du recyclerview
         mRecyclerView = (RecyclerView) root.findViewById(R.id.myRecyclerView);
@@ -77,19 +63,4 @@ public class ListAllArtistsFragment extends Fragment {
         for(Musique m : MainActivity.maMusique) if (m.getAuthor().equals (artiste) ) musiques.add (m);
         return musiques;
     }
-    private ServiceConnection connection = new ServiceConnection() {
-
-        @Override
-        public void onServiceConnected(ComponentName className, IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
-            MusiqueService.LocalBinder binder = (MusiqueService.LocalBinder) service;
-            mService = binder.getService();
-            mBound = true;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            mBound = false;
-        }
-    };
 }
