d1712cf2db057b87001860a699000ce8b85e97c4
clementreiffers <reiffersclement@gmail.com>
2021-05-28 13:10:55 +0200

    correction d'un bug au premier démarage

--- Wolfii/app/src/main/java/com/example/wolfii/MainActivity.java
+++ Wolfii/app/src/main/java/com/example/wolfii/MainActivity.java
@@ -102,52 +102,57 @@ public class MainActivity extends AppCompatActivity {
     // On recupere toutes les musiques disponibles sur le telephone
     public ArrayList getMusic() {
         ArrayList<Musique> maMusique = new ArrayList<Musique>();
-        ContentResolver contentResolver = getContentResolver(); // rechercher toutes les données voulues
-        Uri songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI; // charger les docs externes
-        Cursor songCursor = contentResolver.query(songUri, null, null, null, null);
+        try {
+            ContentResolver contentResolver = getContentResolver(); // rechercher toutes les données voulues
+            Uri songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI; // charger les docs externes
+            Cursor songCursor = contentResolver.query(songUri, null, null, null, null);
 
-        if (songCursor != null && songCursor.moveToFirst()) {
-            // MediaStore permet de lire les metadonnees
-            // on donne le numero de colonne qui correspond a chaque metadonnes qui nous interesse
-            // avec notre curseur
-            int songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
-            int songArtist = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
-            int songLocation = songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
-            int songDuration = songCursor.getColumnIndex(MediaStore.Audio.Media.DURATION);
-            int songDateTaken = songCursor.getColumnIndex(MediaStore.Audio.Media.DATE_TAKEN);
-            int songAlbum = songCursor.getColumnIndex (MediaStore.Audio.Media.ALBUM);
-            int songGenre = 25; // la colonne des genres
-            do {
-                // on recupere une par une certaines metadonnees des nos musiques
-                String currentTitle = songCursor.getString(songTitle);
-                String currentArtist = songCursor.getString(songArtist);
-                String currentPath = songCursor.getString(songLocation);
-                String currentDuration = songCursor.getString(songDuration);
-                String currentDateTaken = songCursor.getString(songDateTaken);
-                String currentGenre = songCursor.getString(songGenre);
-                String currentAlbum = songCursor.getString (songAlbum);
+            if (songCursor != null && songCursor.moveToFirst()) {
+                // MediaStore permet de lire les metadonnees
+                // on donne le numero de colonne qui correspond a chaque metadonnes qui nous interesse
+                // avec notre curseur
+                int songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+                int songArtist = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+                int songLocation = songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
+                int songDuration = songCursor.getColumnIndex(MediaStore.Audio.Media.DURATION);
+                int songDateTaken = songCursor.getColumnIndex(MediaStore.Audio.Media.DATE_TAKEN);
+                int songAlbum = songCursor.getColumnIndex(MediaStore.Audio.Media.ALBUM);
+                int songGenre = 25; // la colonne des genres
+                do {
+                    // on recupere une par une certaines metadonnees des nos musiques
+                    String currentTitle = songCursor.getString(songTitle);
+                    String currentArtist = songCursor.getString(songArtist);
+                    String currentPath = songCursor.getString(songLocation);
+                    String currentDuration = songCursor.getString(songDuration);
+                    String currentDateTaken = songCursor.getString(songDateTaken);
+                    String currentGenre = songCursor.getString(songGenre);
+                    String currentAlbum = songCursor.getString(songAlbum);
 
-                Musique musique = new Musique ();
-                musique.setNomMusique (currentTitle);
-                musique.setAlbum (currentAlbum);
-                musique.setAuthor (currentArtist);
-                musique.setDateTaken (currentDateTaken);
-                musique.setDuration (currentDuration);
-                musique.setGenre (currentGenre);
-                musique.setPath (currentPath);
+                    Musique musique = new Musique();
+                    musique.setNomMusique(currentTitle);
+                    musique.setAlbum(currentAlbum);
+                    musique.setAuthor(currentArtist);
+                    musique.setDateTaken(currentDateTaken);
+                    musique.setDuration(currentDuration);
+                    musique.setGenre(currentGenre);
+                    musique.setPath(currentPath);
 
-                maMusique.add(musique);
-                // on repertorie tous les differents genres dispos
-                if(!mesGenres.contains (currentGenre)) mesGenres.add(currentGenre);
-                if(!mesArtistes.contains(currentArtist)) mesArtistes.add(currentArtist);
-                if(!mesAlbums.contains (currentAlbum)) {
-                    mesAlbums.add(currentAlbum);
-                    mesAlbumsImages.add(currentPath);
-                }
-            } while (songCursor.moveToNext()); // on arrete quand on est arrive a la fin du curseur
+                    maMusique.add(musique);
+                    // on repertorie tous les differents genres dispos
+                    if (!mesGenres.contains(currentGenre)) mesGenres.add(currentGenre);
+                    if (!mesArtistes.contains(currentArtist)) mesArtistes.add(currentArtist);
+                    if (!mesAlbums.contains(currentAlbum)) {
+                        mesAlbums.add(currentAlbum);
+                        mesAlbumsImages.add(currentPath);
+                    }
+                } while (songCursor.moveToNext()); // on arrete quand on est arrive a la fin du curseur
+            }
+            // on reverse le tableau pour avoir les titres telecharges recement en premier
+            Collections.reverse(maMusique);
         }
-        // on reverse le tableau pour avoir les titres telecharges recement en premier
-        Collections.reverse(maMusique);
+        catch (Exception e) {
+            
+        }
         return maMusique;
     }
 
